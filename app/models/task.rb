961972fea7b8afc29315ccd9667f63a0437552f7
Jiyoung Ko <jclareko@gmail.com>
2020-11-10 21:25:24 +0100

    add get next occurrence function

--- app/models/task.rb
+++ app/models/task.rb
@@ -5,15 +5,23 @@ class Task < ApplicationRecord
   belongs_to :room
 
   def set_cycle( number, frequency )
-
     schedule = IceCube::Schedule.new
     case frequency 
     when 'daily'
       schedule.add_recurrence_rule(IceCube::Rule.daily.count(number))
+    when 'weekly'
+      schedule.add_recurrence_rule(IceCube::Rule.weekly.count(number))
+    when 'monthly'
+      schedule.add_recurrence_rule(IceCube::Rule.monthly.count(number))
     end
 
     return schedule
-
   end
+
+  def get_next_occurrence
+    #if task has no occurences, schedule.next_occurrence(from_time)     # defaults to Time.now
+    #if task has previous occurrence, 
+  end
+
 
 end
